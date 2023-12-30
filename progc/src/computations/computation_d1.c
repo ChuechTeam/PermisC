b2adfb3ad9034889e070962b73c4a2fa7fcffbac
jeuxjeux20 <jeuxjeux20@outlook.fr>
2023-12-30 15:41:19 +0100

    VERY FAST hash map implementation for D1 (Experimental!)

--- progc/src/computations/computation_d1.c
+++ progc/src/computations/computation_d1.c
@@ -54,7 +54,149 @@ void llStrAdd(LLStr* list, char* value)
 
 /*
  * ------------
- * ROUTE AVL
+ * ROUTE MAP: A hash map of routes to their drivers.
+ * ------------
+ */
+
+// Key: RouteId
+// Value: Linked list of drivers who have driven this route.
+typedef struct RouteMapEntry
+{
+    int key;
+    bool occupied;
+    LLStr value;
+} RouteMapEntry;
+
+// This route map works using open addressing.
+typedef struct
+{
+    RouteMapEntry* slots;
+    int capacity;
+    int size;
+    float loadFactor;
+    int sizeThreshold;
+} RouteMap;
+
+void routeMapInit(RouteMap* map, int initialCapacity, float loadFactor)
+{
+    assert(initialCapacity > 0);
+    assert(map);
+    assert(loadFactor > 0.0f && loadFactor < 1.0f);
+
+    map->capacity = initialCapacity;
+    map->size = 0;
+    map->loadFactor = loadFactor;
+    map->sizeThreshold = (int) (initialCapacity * loadFactor);
+
+    map->slots = calloc(initialCapacity, sizeof(RouteMapEntry));
+}
+
+// Add some spacing between the keys to avoid collisions, as we're using open addressing.
+// It's sort of like double hashing, but not really since we use the same hash function, just multiplied by 2.
+const int kMapSpacing = 2;
+
+int routeMapHash(int key)
+{
+    return key;
+}
+
+int routeMapFindSlot(RouteMap* map, int key)
+{
+    int i = (kMapSpacing * routeMapHash(key)) % map->capacity;
+    // Continue searching if we come across an occupied slot by some other key
+    while (map->slots[i].occupied && map->slots[i].key != key)
+    {
+        i = (i + 1) % map->capacity;
+    }
+
+    return i;
+}
+
+LLStr* routeMapLookup(RouteMap* map, int key)
+{
+    int slot = routeMapFindSlot(map, key);
+    if (map->slots[slot].occupied)
+    {
+        return &map->slots[slot].value;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+void routeMapGrow(RouteMap* map);
+
+LLStr* routeMapInsert(RouteMap* map, int key, const LLStr* value)
+{
+    int slot = routeMapFindSlot(map, key);
+    assert(!map->slots[slot].occupied);
+
+    // If inserting this element would exceed the load factor threshold, grow!
+    if (map->size + 1 >= map->sizeThreshold)
+    {
+        routeMapGrow(map);
+        slot = routeMapFindSlot(map, key);
+    }
+
+    map->slots[slot].value = *value;
+    map->slots[slot].key = key;
+    map->slots[slot].occupied = true;
+
+    map->size++;
+
+    return &map->slots[slot].value;
+}
+
+void routeMapGrow(RouteMap* map)
+{
+    int prevCapacity = map->capacity;
+    int nextCapacity = map->capacity * 2;
+    while ((int) (nextCapacity * map->loadFactor) <= map->size + 1)
+    {
+        nextCapacity *= 2;
+    }
+    RouteMapEntry* prevSlots = map->slots;
+    RouteMapEntry* nextSlots = calloc(nextCapacity, sizeof(RouteMapEntry));
+
+    map->capacity = nextCapacity;
+    map->sizeThreshold = (int) (nextCapacity * map->loadFactor);
+    map->slots = nextSlots;
+
+    // Put all elements back in the new slots.
+    map->size = 0;
+    for (int i = 0; i < prevCapacity; ++i)
+    {
+        RouteMapEntry entry = prevSlots[i];
+        if (entry.occupied)
+        {
+            routeMapInsert(map, entry.key, &entry.value);
+        }
+    }
+
+    free(prevSlots);
+}
+
+typedef void (*RouteMapValueDestructFunc)(LLStr* value);
+
+void routeMapFree(RouteMap* map, RouteMapValueDestructFunc destruct)
+{
+    assert(map);
+    assert(destruct);
+
+    for (int i = 0; i < map->capacity; ++i)
+    {
+        RouteMapEntry* entry = &map->slots[i];
+        if (entry->occupied)
+        {
+            destruct(&entry->value);
+        }
+    }
+}
+
+/*
+ * ------------
+ * ROUTE AVL (KEPT FOR REFERENCE, IT IS NOT USED ANYMORE IN THIS BRANCH!)
  * ------------
  */
 
@@ -183,6 +325,24 @@ void freeBestDriversAVL(DriverSortAVL* node);
 
 void freeRoutesAVL(RouteAVL* node);
 
+void freeRouteMapValue(LLStr* val)
+{
+    if (!val)
+    {
+        return;
+    }
+
+    // Free the allocated linked list nodes (second and later).
+    // We do not need to free the strings as they are in the drivers AVL, which will be freed later on.
+    LLStr* it = val->next;
+    while (it != NULL)
+    {
+        LLStr* next = it->next;
+        free(it);
+        it = next;
+    }
+}
+
 void computationD1(RouteStream* stream)
 {
     // The intermediate AVL containing all routes, for lookup.
@@ -190,7 +350,7 @@ void computationD1(RouteStream* stream)
     //
     // You can think of it as a dictionary, or a function/map:
     //     f(routeId) -> [driverName1, driverName2, ...]
-    RouteAVL* routes = NULL;
+    //RouteAVL* routes = NULL;
 
     // The AVL containing all drivers by their name, with the number of routes they have taken
     // (in extraData, with the DriverData struct), for lookup.
@@ -203,6 +363,10 @@ void computationD1(RouteStream* stream)
     //    f(driverName) -> routeCount
     StringAVL* drivers = NULL;
 
+    // Replaces the AVL method.
+    RouteMap map;
+    routeMapInit(&map, 256, 0.5f);
+
     // Phase 1: Read all the route steps
     // ------------------------------------------
     // Let's read all the route steps and fill our two intermediate AVLs with data.
@@ -214,12 +378,19 @@ void computationD1(RouteStream* stream)
         while (rsRead(stream, &step, ROUTE_ID | DRIVER_NAME))
         {
             // Insert the route into the route AVL, or find the existing node.
-            RouteAVL* routeInfo = NULL;
-            routes = routeAVLInsert(routes, &step.routeId, &routeInfo, NULL);
+            //RouteAVL* routeInfo = NULL;
+            //routes = routeAVLInsert(routes, &step.routeId, &routeInfo, NULL);
+
+            LLStr* driversInRoute = routeMapLookup(&map, step.routeId);
+            if (!driversInRoute)
+            {
+                LLStr list = {NULL, NULL};
+                driversInRoute = routeMapInsert(&map, step.routeId, &list);
+            }
 
             // Has the driver already been seen on this route?
             bool driverAlreadySeen = false;
-            LLStr* it = &routeInfo->drivers;
+            LLStr* it = driversInRoute;
             // Traverse the linked list of drivers already assigned to this route.
             while (it && it->value != NULL)
             {
@@ -253,7 +424,7 @@ void computationD1(RouteStream* stream)
                 // Add it to the list of drivers involved in this route.
                 // Use the string located in the AVL since it is valid through the entire computation.
                 char* driverName = driverNode->value;
-                llStrAdd(&routeInfo->drivers, driverName);
+                llStrAdd(driversInRoute, driverName);
             }
         }
 
@@ -291,12 +462,14 @@ void computationD1(RouteStream* stream)
         // other than its child nodes it has nothing else to free.
         freeBestDriversAVL(bestDrivers);
 
+        routeMapFree(&map, &freeRouteMapValue);
+
         // The routes AVL needs to free all of its nodes,
         // and all of the linked lists in those nodes as well.
         //
         // The strings in the linked lists do not need to be freed because they
         // belong to the drivers AVL, which we're going to free just after.
-        freeRoutesAVL(routes);
+        //freeRoutesAVL(routes);
 
         // Finally, the drivers AVL can be freed using the stringAVLFree function,
         // which is going to free all the nodes and all the extraData we've allocated.
